202ceb38ec9b6146232d945c5d517d0cda5dbf25
tlantz <tim.lantz@gmail.com>
2014-03-02 08:14:51 -0500

    sound player impls

---
+++ src/com/wordpress/tslantz/stringendo/JLayerSoundPlayer.java
@@ -0,0 +1,33 @@
+package com.wordpress.tslantz.stringendo;
+
+final class JLayerSoundPlayer implements SoundPlayer {
+
+	@Override
+	public Track load(String path) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	private final class Track implements SoundPlayer.Track {
+
+		@Override
+		public void loop(int startMSec, int endMSec, double speed) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void pause() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void stop() {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+
+}
---
+++ src/com/wordpress/tslantz/stringendo/OpenSLSoundPlayer.java
@@ -0,0 +1,11 @@
+package com.wordpress.tslantz.stringendo;
+
+final class OpenSLSoundPlayer implements SoundPlayer {
+
+	@Override
+	public Track load(String path) {
+		// TODO write opensl implementation
+		return null;
+	}
+	
+}
